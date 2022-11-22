d34533a2fbb7b5d9155d1190198e2f6f7001617d
Potchara Puttawanchai <potchangelo@gmail.com>
2022-11-22 16:19:22 +0700

    Try functions definitions

--- index.d.ts
+++ index.d.ts
@@ -2,10 +2,16 @@ export interface ThaiWordOptions {
   showActual?: boolean
 }
 
+/**
+ * Convert money (number) to Thai word (string).
+ */
 export function moneyToThaiWord(money: number, options?: ThaiWordOptions): string;
 
 export interface ThaiWordLocaleENOptions {
   textTransform?: 'first-letter'|'lowercase'|'uppercase'|'capitalize'
 }
 
+/**
+ * Convert money (number) to Thai word (string) in English locale.\
+ */
 export function moneyToThaiWordLocaleEN(money: number, options?: ThaiWordLocaleENOptions): string;
